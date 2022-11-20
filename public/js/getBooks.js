7b685b1a250137447c5ad09222a3ac3e8192f66c
tmchuynh <tinamchuynh@gmail.com>
2022-11-20 14:06:07 -0800

    update accordion style and upload importing info of book

--- public/js/getBooks.js
+++ public/js/getBooks.js
@@ -5,12 +5,24 @@ $(document).ready(function () {
         switch (c[i].innerHTML) {
             case "Art":
                 $.get("https://openlibrary.org/subjects/art.json?limit=100", function (data) {
-                    console.log(data.works[i].authors);
+                    console.log(data.works[i].availability.isbn);
                     const author = [];
+                    const subject = [];
+
+                    for (var k = 0; k < data.works[i].authors.length; k++) {
+                        // console.log(data.works[i].authors[k].name);
+                        author.push(" " +data.works[i].authors[k].name + " ");
+                    }
+                    console.log(author);
+
+                    for (var j = 0; j < data.works[i].subject.length; j++) {
+                        subject.push(" " + data.works[i].subject[j] + " ");
+                    }
+
                     
                     // newBook("title", "Micheal", 4568321, "music, classical", "headingTen", "collapseTen");
                     // while(data.works) {
-                    // newBook(data.works[i].title, author, "00000000", data.works.subject, "heading", "collapse");
+                    newBook(data.works[i].title, author, data.works[i].availability.isbn, subject, "heading", "collapse");
 
                     // }
                 })
@@ -68,6 +80,7 @@ function newBook(title, authorName, isbnNum, keywordList, headingNum, target) {
     header.classList.add("accordion-header");
     header.setAttribute("id", headingNum)
     item.appendChild(header);
+
     button = document.createElement("button");
     button.classList.add("accordion-button");
     button.classList.add("collapsed");
@@ -77,6 +90,7 @@ function newBook(title, authorName, isbnNum, keywordList, headingNum, target) {
     button.setAttribute("aria-controls", target);
     button.innerHTML = String(title);
     header.appendChild(button);
+
     info = document.createElement("div");
     info.setAttribute("id", target);
     info.classList.add("accordioan-collapse");
@@ -84,30 +98,35 @@ function newBook(title, authorName, isbnNum, keywordList, headingNum, target) {
     info.setAttribute("aria-lablledby", headingNum);
     info.setAttribute("data-bs-parent", "#accordionExample");
     item.appendChild(info);
+
     body = document.createElement("div");
     body.classList.add("accordion-body");
     info.appendChild(body);
+
     p1 = document.createElement("p");
     author = document.createElement("strong");
     author.classList.add("author");
     author.innerHTML = "Author: ";
-    p1.appendChild(author);
-    p1.innerHTML = author.innerHTML + authorName + "<br>";
+    p1.innerHTML = authorName + "<br>";
+    body.appendChild(author);
     body.appendChild(p1);
+
     p2 = document.createElement("p");
     isbn = document.createElement("strong");
     isbn.classList.add("isbn");
-    p2.appendChild(isbn);
     isbn.innerHTML = "ISBN: ";
-    p2.innerHTML = isbn.innerHTML + isbnNum + "<br>";
+    p2.innerHTML =  isbnNum + "<br>";
+    body.appendChild(isbn);
     body.appendChild(p2);
+
     p3 = document.createElement("p");
     keywords = document.createElement("strong");
     keywords.classList.add("keywords");
     keywords.innerHTML = "Keywords: ";
-    p3.appendChild(keywords);
-    p3.innerHTML = keywords.innerHTML + keywordList;
+    p3.innerHTML =  keywordList;
+    body.appendChild(keywords);
     body.appendChild(p3);
+
     console.log(item);
     a.appendChild(item);
 }
